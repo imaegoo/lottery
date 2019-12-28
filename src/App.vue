8551ce9416ab9956e7c5f31de10a7decb965b947
Jimmy Ma <jimmyma@augmentum.com.cn>
2019-12-28 20:48:28 +0800

    lottery init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Setting />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Setting from './components/Setting.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Setting
+  },
+  data () {
+    return {
+      page: ''
+    }
+  },
+  mounted () {
+    this.page = 'setting'
   }
 }
 </script>
---
+++ src/components/Setting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>请设置抽奖池</h1>
+    <h2>一行一个</h2>
+    <textarea v-model="numbers" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      numbers: ''
+    }
+  }
+}
+</script>
